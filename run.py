9c8e304782070f75ca67e29304f4b7fb9fd386f5
Jason Liang <jjjasonliang@gmail.com>
2020-07-01 20:07:56 +0800

    https flask

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 from EyeTracking import app
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    context  = ('cert/server.crt', 'cert/server.key')
+    app.run(debug=True, ssl_context=context)
