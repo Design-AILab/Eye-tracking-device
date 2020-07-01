09515c6fd04f0764ced27edb8cf00ca72826ae3f
Jason Liang <jjjasonliang@gmail.com>
2020-07-01 20:42:58 +0800

    try to obtain datapoints

--- run.py
+++ run.py
@@ -2,4 +2,4 @@ from EyeTracking import app
 
 if __name__ == "__main__":
     context  = ('cert/server.crt', 'cert/server.key')
-    app.run(debug=True, ssl_context=context)
+    app.run('0.0.0.0', port=8100, debug=True, ssl_context=context)
