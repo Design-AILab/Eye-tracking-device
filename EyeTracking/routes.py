fb235d480c5f0787f9f62ae1e56b3d4dc07211f0
Jason Liang <jjjasonliang@gmail.com>
2020-07-02 17:20:48 +0800

    ajax send data

--- EyeTracking/routes.py
+++ EyeTracking/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, url_for, flash, redirect, request
+from flask import Flask, render_template, url_for, flash, redirect, request, jsonify
 from EyeTracking import app, db
 import os
 
@@ -9,3 +9,18 @@ def index():
 @app.route('/tracking')
 def tracking():
 	return render_template('tracking.html')
+
+
+@app.route('/tracked_coordinates', methods=['POST'])
+def tracked_coordinates():
+	# print(request.json)
+	coords = request.json['data']
+	print(coords)
+	data = coords
+	#return redirect(url_for('show_coords', length=len(data), data=data))
+	return render_template('results.html',  length=len(data), data=data)
+
+
+# @app.route('/show_results/<length>/<data>')
+# def show_coords(length, data):
+# 	return render_template('results.html', length=int(length), data=data)
